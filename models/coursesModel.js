761136fa8f3d482b5fcd81388ba5a092c662c381
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2026-03-03 11:45:36 -0300

    "MongoNotConnectedError: Client must be connected before running operations" problem fixed

--- models/coursesModel.js
+++ models/coursesModel.js
@@ -9,7 +9,6 @@ async function getAllCourses() {
         const db = getDB();
         const coursesCollection = db.collection('courses');
         const courses = await coursesCollection.find({}).toArray();
-        await db.client.close();
         return courses;
     } catch(error) {
         console.error('Error fetching courses:', error);
@@ -26,11 +25,9 @@ async function getCourseById(courseId) {
         const course = await coursesCollection.findOne({ _id: objectId });
         if(course != null) {
             //console.log('Course fetched successfully:', course);
-            await db.client.close();
             return course;
         } else{
             console.log('Course not found with ID:', courseId);
-            await db.client.close();
             return null;
         }
     } catch(error) {
@@ -46,7 +43,6 @@ async function getCoursesByLevel(level) {
         const db = getDB();
         const coursesCollection = db.collection('courses');
         const courses = await coursesCollection.find({ level: level }).toArray();
-        await db.client.close();
         return courses;
     } catch(error) {
         console.error('Error fetching courses:', error);
@@ -61,7 +57,6 @@ async function getCoursesByCategory(category) {
         const db = getDB();
         const coursesCollection = db.collection('courses');
         const courses = await coursesCollection.find({ category: category }).toArray();
-        await db.client.close();
         return courses;
     } catch(error) {
         console.error('Error fetching courses:', error);
--- models/lessonsModel.js
+++ models/lessonsModel.js
@@ -8,7 +8,6 @@ async function getAllLessons() {
         const db = getDB();
         const lessonsCollection = db.collection('lessons');
         const lessons = await lessonsCollection.find({}).toArray();
-        await db.client.close();
         return lessons; 
     } catch(error) {
         console.error('Error fetching lessons:', error);
@@ -23,7 +22,6 @@ async function getLessonsByCourseId(courseId) {
         const lessonsCollection = db.collection('lessons');
         const objectId = new ObjectId(courseId);
         const lessons = await lessonsCollection.find({ courseId: objectId }).toArray();
-        await db.client.close();
         return lessons; 
     } catch(error) {
         console.error('Error fetching lessons:', error);
@@ -39,7 +37,6 @@ async function getNextLesson(lessonId) {
         const currentCourse = await lessonsCollection.findOne({ _id: new ObjectId(lessonId) });
         const nextLesson = await lessonsCollection.findOne({ _id: new ObjectId(currentCourse.nextStepId) });
         console.log('Next lesson fetched successfully:', nextLesson);
-        await db.client.close();
         return nextLesson;
     } catch(error) {
         console.error('Error fetching lessons:', error);
@@ -55,7 +52,6 @@ async function getPrevLesson(lessonId) {
         const currentCourse = await lessonsCollection.findOne({ _id: new ObjectId(lessonId) });
         const prevLesson = await lessonsCollection.findOne({ _id: new ObjectId(currentCourse.prevStepId) });
         console.log('prev:', prevLesson);
-        await db.client.close();
         return prevLesson;
     } catch(error) {
         console.error('Error fetching lessons:', error);
